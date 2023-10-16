7fe4bb19e33b04dc62e247fa288e438525408f16
FloyzenCode <vasyplay323@gmail.com>
2023-10-17 01:36:56 +0400

    Add Shell scripts for optimization and effective run binary file

---
+++ client.sh
@@ -0,0 +1,3 @@
+cd bin
+echo "Start client.sh"
+./route_guide_client --db_path=../../route_guide_db.json
---
+++ server.sh
@@ -0,0 +1,3 @@
+cd bin
+echo "Start server.sh"
+./route_guide_server --db_path=../../route_guide_db.json
